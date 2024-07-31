789540de32f200ce402ecfd1ed243879c4f873b0
Abrar Tajwar Khan <abrar.tajwar2@gmail.com>
2024-07-31 09:48:40 +0600

    Adding new code

---
+++ Vector/Sorting Vector.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main () {
+    int n;
+    cout<<"Enter array number: ";
+    cin>>n;
+    vector<int>v(n);
+    cout<<"Enter your digits: ";
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    sort(v.begin(),v.end());
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+
+}
+}
